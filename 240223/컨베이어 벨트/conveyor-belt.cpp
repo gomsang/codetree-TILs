81324b3825c772d7a34a8bb0f95e17195b3152d4
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-02-23 02:11:47 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define MAX_N 200
+
+using namespace std;
+
+int belt[MAX_N * 2];
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    for(int i = 0; i < n * 2; i++) {
+        cin >> belt[i];
+    }
+
+    for(int i = 0; i < t; i++) {
+        int last = belt[n * 2 - 1];
+        for(int i = n * 2 - 1; i > 0; i--) {
+            belt[i] = belt[i - 1];
+        }
+        belt[0] = last;
+    }
+
+   for(int i = 0; i < n * 2; i++) {
+        cout << belt[i] << " ";
+    }
+    
+
+    return 0;
+}
