543657795bc472244fbdb46269d61964eb6783b1
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-02-23 15:44:55 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/1차원 젠가/jenga-1d.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define LIST_MAX 100
+
+int stack[LIST_MAX];
+
+
+int delBlock(int n, int s, int e) {
+    for(int i = n - e; i < (n - (e - s + 1)); i++) {
+        stack[i] = stack[n-s + (i - (n - e))];
+    }
+    return e - s + 1;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> stack[n - i - 1];
+    }
+
+    int s, e;
+    cin >> s >> e;
+    n -= delBlock(n, s, e);
+
+    cin >> s >> e;
+    n -= delBlock(n, s, e);
+
+    cout << n << endl;
+    for(int i = n - 1; i >= 0; i--){
+        cout << stack[i] << endl;
+    }
+    return 0;
+}
