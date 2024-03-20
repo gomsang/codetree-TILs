b8a9f8c8e1108f0c747155a4a7368d6e4c00f044
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-03-20 16:41:55 +0900

    Title: 양수 직사각형의 최대 크기, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240320/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -2,7 +2,7 @@ N, M = map(int, input().split())
 
 grid = []
 
-negative_numbers = []
+non_positive = []
 
 for _ in range(N):
     grid.append(list(map(int, input().split())))
@@ -10,11 +10,11 @@ for _ in range(N):
 for row in range(N):
     for col in range(M):
         if grid[row][col] <= 0:
-            negative_numbers.append([row, col])
+            non_positive.append([row, col])
 
 
-def having_negative(s_row, s_col, e_row, e_col):
-    for n_row, n_col in negative_numbers:
+def having_non_positive(s_row, s_col, e_row, e_col):
+    for n_row, n_col in non_positive:
         if s_row <= n_row <= e_row and s_col <= n_col <= e_col:
             return True
     return False
@@ -23,13 +23,13 @@ def having_negative(s_row, s_col, e_row, e_col):
 def check(width, height):
     for grid_row in range(N - height + 1):
         for grid_col in range(M - width + 1):
-            if not having_negative(grid_row, grid_col, grid_row + height - 1, grid_col + width - 1):
+            if not having_non_positive(grid_row, grid_col, grid_row + height - 1, grid_col + width - 1):
                 return True
     return False
 
 
 def run():
-    if not negative_numbers:
+    if not non_positive:
         return N * M
 
     score = -1
