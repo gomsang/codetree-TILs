aefd0f681454feabb7d2d36101e1d1c9eac874e8
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-03-21 03:32:25 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240321/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -3,17 +3,12 @@ n, r, c = map(int, input().split())
 r -= 1
 c -= 1
 
-direction = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 grid = []
 
-numbers = []
-
 for _ in range(n):
     grid.append(list(map(int, input().split())))
-
-numbers.append(grid[r - 1][c - 1])
-
 
 def find():
     global r, c
@@ -22,7 +17,6 @@ def find():
             if grid[r + dir[0]][c + dir[1]] > grid[r][c]:
                 r = r + dir[0]
                 c = c + dir[1]
-                numbers.append(grid[r][c])
                 return True
     return False
 
