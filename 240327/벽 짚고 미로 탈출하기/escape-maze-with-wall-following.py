15da265b7c2718ba397a67389fd7bb2521bd62d4
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-03-27 01:28:55 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240327/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -11,11 +11,13 @@ grid = []
 for row in range(n):
     grid.append(list(input()))
 
+
 def avilable(x, y):
     if not (1 <= x <= n and 1 <= y <= n): return True
     if grid[y - 1][x - 1] == "#":
         return False
     return True
+
 
 def rightwallchk(x, y):
     if not (1 <= x <= n and 1 <= y <= n): return True
@@ -26,11 +28,8 @@ def rightwallchk(x, y):
 
 iso_cnt = 0
 t = 0
-while(True):
+while (True):
     if iso_cnt >= 4:
-        t = -1
-        break
-    if not rightwallchk(x + direction_grid[(direction + 1) % 4][0], y + direction_grid[(direction + 1) % 4][1]):
         t = -1
         break
     if avilable(x + direction_grid[direction][0], y + direction_grid[direction][1]):
@@ -38,10 +37,14 @@ while(True):
         if not (1 <= x + direction_grid[direction][0] <= n and 1 <= y + direction_grid[direction][1] <= n):
             t += 1
             break
-        if not avilable(x + direction_grid[direction][0] + direction_grid[(direction + 1) % 4][0], y + direction_grid[direction][1] + direction_grid[(direction + 1) % 4][1]):
+        if not avilable(x + direction_grid[direction][0] + direction_grid[(direction + 1) % 4][0],
+                        y + direction_grid[direction][1] + direction_grid[(direction + 1) % 4][1]):
             t += 1
             x = x + direction_grid[direction][0]
             y = y + direction_grid[direction][1]
+            if not rightwallchk(x + direction_grid[(direction + 1) % 4][0], y + direction_grid[(direction + 1) % 4][1]):
+                t = -1
+                break
         else:
             x = x + direction_grid[direction][0]
             y = y + direction_grid[direction][1]
@@ -49,6 +52,9 @@ while(True):
             x = x + direction_grid[direction][0]
             y = y + direction_grid[direction][1]
             t += 2
+            if not rightwallchk(x + direction_grid[(direction + 1) % 4][0], y + direction_grid[(direction + 1) % 4][1]):
+                t = -1
+                break
     else:
         iso_cnt += 1
         direction = (direction - 1) % 4
