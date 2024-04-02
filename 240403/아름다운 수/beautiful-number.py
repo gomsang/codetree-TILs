428e812ade58c200bb4dff97e9d1d53336350416
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-03 03:33:42 +0900

    Title: 아름다운 수, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/아름다운 수/beautiful-number.py
+++ 240403/아름다운 수/beautiful-number.py
@@ -5,7 +5,7 @@ def choose(text):
     global cnt
     if len(text) == n:
         cnt = cnt + 1
-    for num in range(1, n - len(text) + 1):
+    for num in range(1, min(5, n - len(text) + 1)):
         choose(text + str(num) * num)
 
 choose("")
