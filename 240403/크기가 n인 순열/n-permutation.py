2df76fa7dbe9af30e106b908530d261dc1da48e4
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-03 14:36:15 +0900

    Title: 크기가 n인 순열, Time: 267ms, Memory: 29MB, Status: Passed - CodeTree

--- 240403/크기가 n인 순열/n-permutation.py
+++ 240403/크기가 n인 순열/n-permutation.py
@@ -8,10 +8,10 @@ def visit(arr):
     for v in range(n):
         if not visited[v]:
             visited[v] = True
-            visit(arr + [v + 1], v)
+            visit(arr + [v + 1])
             visited[v] = False
 
 for num in range(n):
     visited[num] = True
-    visit([num + 1], num)
+    visit([num + 1])
     visited[num] = False
