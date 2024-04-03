a5edf3d3a17ea221e9585e50beff4c702e198cef
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-04 01:27:45 +0900

    Title: 겹치지 않게 선분 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240404/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,34 @@
+n = int(input())
+segments = []
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    segments.append((x1, x2))
+
+selected_segments = list()
+
+def is_overlap(segment1, segment2):
+    (ax1, ax2), (bx1, bx2) = segment1, segment2
+    return (ax1 <= bx1 <= ax2) or (ax1 <= bx2 <= ax2) or (bx1 <= ax1 <= bx2) or (bx1 <= ax2 <= bx2)
+
+def possible():
+    for idx_a, segment1 in enumerate(selected_segments):
+            for idx_b, segment2 in enumerate(selected_segments):
+                if idx_a < idx_b and is_overlap(segment1, segment2):
+                    return False
+    return True
+
+selectmax = 0
+
+def check(select):
+    global selectmax
+    if select == n and possible():
+        selectmax = max(selectmax, select)
+        return
+    
+    selected_segments.append(segments[select])
+    check(select + 1)
+    selected_segments.pop()
+    check(select + 1)
+
+check(0)
+print(selectmax)
