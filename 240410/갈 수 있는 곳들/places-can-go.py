477e43ea8d335bd8d07cc04ce275d8976e5b0e25
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-10 01:02:08 +0900

    Title: 갈 수 있는 곳들, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240410/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+grid = []
+
+directions = [(1,0), (-1,0), (0,-1), (0, 1)]
+
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+
+q = deque()
+
+def in_range(row, col):
+    return 0 <= row < n and 0 <= col < n
+
+for _ in range(k):
+    q.append(map(int, input().split()))
+
+while q:
+    row, col = q.popleft()
+    for direction in directions:
+        r, c = row + direction[0], col + direction[1]
+        if not in_range(r,c):
+            continue
+        if grid[r][c] == 1 or visited[r][c]:
+            continue
+        visited[r][c] = True
+        q.append((r,c))
+
+visitedSpace = 0
+for row in visited:
+    visitedSpace += sum(row)
+
+print(visitedSpace)
