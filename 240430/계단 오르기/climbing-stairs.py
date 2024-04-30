6f966b9cffc01c967d480cbfb221a30f5b3c4f25
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-30 23:26:14 +0900

    Title: 계단 오르기, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240430/계단 오르기/climbing-stairs.py
+++ 240430/계단 오르기/climbing-stairs.py
@@ -1,17 +1,34 @@
 n = int(input())
 
-dp = [0] * (n + 1)
-
+dp = [-1] * (n + 1)
 dp[0] = 1
 if n >= 2:
- dp[2] = 1
+    dp[2] = 1
 if n >= 3:
- dp[3] = 1
+    dp[3] = 1
 
-for i in range(4, n + 1):
-    if dp[i - 2] > 0:
-        dp[i] += dp[i - 2]
-    if dp[i - 3] > 0:
-        dp[i] += dp[i - 3]
+def process(n):
+    if dp[n] > -1:
+        return dp[n]
+    dp[n] = process[n-2] + process[n-3]
+
 
 print(dp[n] % 10007)
+
+# n = int(input())
+
+# dp = [0] * (n + 1)
+
+# dp[0] = 1
+# if n >= 2:
+#  dp[2] = 1
+# if n >= 3:
+#  dp[3] = 1
+
+# for i in range(4, n + 1):
+#     if dp[i - 2] > 0:
+#         dp[i] += dp[i - 2]
+#     if dp[i - 3] > 0:
+#         dp[i] += dp[i - 3]
+
+# print(dp[n] % 10007)
