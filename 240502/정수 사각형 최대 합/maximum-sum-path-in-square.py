435fa64fb36ad02470ffbbfbb3df3b8625f1df34
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-05-02 01:50:51 +0900

    Title: 정수 사각형 최대 합, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240502/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+a = []
+
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+dp = [
+    [0 for _ in range(n)] for _ in range(n)
+]
+
+dp[0][0] = a[0][0]
+
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0] + a[i][0]
+    dp[0][i] = dp[0][i-1] + a[0][i]
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i][j - 1] + a[i][j],  dp[i - 1][j] + a[i][j])
+
+print(dp[n-1][n-1])
