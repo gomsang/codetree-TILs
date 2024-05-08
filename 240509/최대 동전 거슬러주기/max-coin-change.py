c9a46ef8912ee9aa01b433dc4bd426b6cdf66142
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-05-09 06:06:35 +0900

    Title: 최대 동전 거슬러주기, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,18 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+N, M = map(int, input().split())
+
+coins = list(map(int, input().split()))
+
+dp = [INT_MIN] * (M + 1)
+dp[0] = 0
+
+for coin in coins:
+    for j in range(coin, M + 1):
+        if dp[j - coin] != INT_MIN:
+            continue
+        dp[j] = max(dp[j], dp[j - coin] + 1)
+
+print(dp[M] if dp[M] != INT_MIN else -1)
