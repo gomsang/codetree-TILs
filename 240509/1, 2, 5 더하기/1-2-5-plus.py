de985efa9422617e3caf0f7c7309bc5fb512aafc
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-05-09 05:56:48 +0900

    Title: 1, 2, 5 더하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240509/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+dp = [0] * (n+1)
+
+dp[0] = 1
+
+
+for i in range(1, n + 1):
+    if i >= 1:
+        dp[i] = (dp[i] + dp[i - 1])
+    if i >= 2:
+        dp[i] = (dp[i] + dp[i - 2])
+    if i >= 5:
+        dp[i] = (dp[i] + dp[i - 5])
+
+print(dp[n] % 10007)
