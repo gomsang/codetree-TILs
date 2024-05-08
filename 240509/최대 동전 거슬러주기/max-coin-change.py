57fc2581d642ca13a0087d76c849709ceaf14ba2
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-05-09 08:57:53 +0900

    Title: 최대 동전 거슬러주기, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240509/최대 동전 거슬러주기/max-coin-change.py
+++ 240509/최대 동전 거슬러주기/max-coin-change.py
@@ -9,14 +9,10 @@ coins = list(map(int, input().split()))
 dp = [INT_MIN] * (M + 1)
 dp[0] = 0
 
-for coin in reversed(coins):
+for coin in coins:
     for j in range(coin, M + 1):
         if dp[j - coin] == INT_MIN:
             continue
         dp[j] = max(dp[j], dp[j - coin] + 1)
 
 print(dp[M] if dp[M] != INT_MIN else -1)
-
-
-# 낮은 단위의 동전 부터 채우기
-# 3 4 5 -> 3 x 1 3 x 2 3 x 3
